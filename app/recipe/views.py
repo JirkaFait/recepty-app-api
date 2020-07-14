fe974fae3d12ee71cab6894572bed511e947e04e
jiri.fait <fait@orkasolutions.cz>
2020-07-14 21:45:20 +0200

    Added filtering testing feature

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -30,11 +30,37 @@ class TagViewSet(BaseRecipeAttrViewSet):
     queryset = Tag.objects.all()
     serializer_class = serializers.TagSerializer
 
+    def get_queryset(self):
+        """Return objects for current user"""
+        assigned_only = bool(
+            int(self.request.query_params.get('assigned_only', 0))
+        )
+        queryset = self.queryset
+        if assigned_only:
+            queryset = queryset.filter(recipe__isnull=False)
+
+        return queryset.filter(
+            user=self.request.user
+        ).order_by('-name').distinct()
+
 
 class IngredientViewSet(BaseRecipeAttrViewSet):
     """Manage ingredients in the database"""
     queryset = Ingredient.objects.all()
     serializer_class = serializers.IngredientSerializer
+
+    def get_queryset(self):
+        """Return objects for current user"""
+        assigned_only = bool(
+            int(self.request.query_params.get('assigned_only', 0))
+        )
+        queryset = self.queryset
+        if assigned_only:
+            queryset = queryset.filter(recipe__isnull=False)
+
+        return queryset.filter(
+            user=self.request.user
+        ).order_by('-name').distinct()
 
 
 class RecipeViewSet(viewsets.ModelViewSet):
